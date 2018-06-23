08395e23658995c62a6cdc861d3840f7d055cd5e
Antony <a.ibbotsonz@gmail.com>
2018-06-23 17:49:39 +0100

    Added POSt /todos route and mongoose refactor

---
+++ server/server.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose.js');
+let { Todo } = require('./models/todo');
+let { User } = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server has started...');
+});
